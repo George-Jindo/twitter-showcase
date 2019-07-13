e1b80b6936a6720b601e575bee57993ca479fe5c
George J <g.jindo@gmail.com>
2019-07-13 08:59:52 -0700

    changed index.js to index.html in serve static file, also created build production of React app

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const port = process.env.PORT || 5000;
 
 // console.log that your server is up and running
@@ -10,7 +11,5 @@ app.use('/static', express.static(path.join(__dirname, 'react')));
 
 // create a GET route
 app.get('/', (req, res) => {
-    res.send({
-        express: "Oh Hey Look, It's Working"
-    });
+    res.sendFile(path.join(__dirname + 'react/build.index.html'));
 });

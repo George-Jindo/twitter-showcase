b8320f2f0f131d7dd01c141ae043a237fce4adb0
George J <g.jindo@gmail.com>
2019-07-13 08:53:54 -0700

    Added static server file code to server.js

--- server.js
+++ server.js
@@ -5,8 +5,11 @@ const port = process.env.PORT || 5000;
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
+// serve static build files from React app
+app.use('/static', express.static(path.join(__dirname, 'react')));
+
 // create a GET route
-app.get('/express_backend', (req, res) => {
+app.get('/', (req, res) => {
     res.send({
         express: "Oh Hey Look, It's Working"
     });

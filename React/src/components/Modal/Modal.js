d8c89628971faeffc50c1b3cd1ae789c300b5736
George J <g.jindo@gmail.com>
2019-08-15 17:40:20 -0700

    added styling and imported remaing 5 tweets within the css grid.

--- React/src/components/Modal/Modal.js
+++ React/src/components/Modal/Modal.js
@@ -8,30 +8,57 @@ const modal = props => (
         <header className='modal_header'>
             <h1>{props.title}</h1>
         </header>
-        <section className="modal_content">
-            <div id="tweet_column">
-                <div className="tweet">
-                    <div className="tweet_image">
-                        <img src={props.tweets.length > 0 ? props.tweets[0].user.profile_image_url_https : ''} alt="Profile Image"></img>
-                    </div>
-                    <div className="tweet_screen_name">
-                        <p>{props.tweets.length > 0 ? props.tweets[0].user.screen_name : ''}</p>
-                    </div>
-                    <div className="tweet_name">
-                        <p>{props.tweets.length > 0 ? props.tweets[0].user.name : ''}</p>
-                    </div>
-                    <hr></hr>
-                    <div className="tweet_text">
-                        <p>{props.tweets.length > 0 ? props.tweets[0].text : ''}</p>
-                    </div>
-                    <hr></hr>
-                    <div className="tweet_date">
-                        <p>{props.tweets.length > 0 ? props.tweets[0].created_at : ''}</p>
-                    </div>
-                    <hr></hr>
-                </div>
+        <div id="tweet_container">
+
+            <div className="Tweet_1">
+                <div className="name_top">{props.tweets.length > 0 ? props.tweets[0].user.name : ''}</div>
+                <div className="screen_name_top">{props.tweets.length > 0 ? '@' + props.tweets[0].user.screen_name : ''}</div>
+                <div className="divider_top"></div>
+                <div className="text_top">{props.tweets.length > 0 ? props.tweets[0].text : ''}</div>
+                <div className="Tweet_img_1"><img src={props.tweets.length > 0 ? props.tweets[0].user.profile_image_url_https : ''} alt="Profile Image"></img></div>
             </div>
-        </section>
+
+            <div className="Tweet_2">
+                <div className="name_top">{props.tweets.length > 1 ? props.tweets[1].user.name : ''}</div>
+                <div className="screen_name_top">{props.tweets.length > 1 ? '@' + props.tweets[1].user.screen_name : ''}</div>
+                <div className="divider_top"></div>
+                <div className="text_top">{props.tweets.length > 1 ? props.tweets[1].text : ''}</div>
+                <div className="Tweet_img_2"><img src={props.tweets.length > 1 ? props.tweets[1].user.profile_image_url_https : ''} alt="Profile Image"></img></div>
+            </div>
+
+            <div className="Tweet_3">
+                <div className="name_top">{props.tweets.length > 2 ? props.tweets[2].user.name : ''}</div>
+                <div className="screen_name_top">{props.tweets.length > 2 ? '@' + props.tweets[2].user.screen_name : ''}</div>
+                <div className="divider_top"></div>
+                <div className="text_top">{props.tweets.length > 2 ? props.tweets[2].text : ''}</div>
+                <div className="Tweet_img_3"><img src={props.tweets.length > 2 ? props.tweets[2].user.profile_image_url_https : ''} alt="Profile Image"></img></div>
+            </div>
+
+            <div className="Tweet_4">
+                <div className="name_bottom">{props.tweets.length > 3 ? props.tweets[3].user.name : ''}</div>
+                <div className="screen_name_bottom">{props.tweets.length > 3 ? '@' + props.tweets[3].user.screen_name : ''}</div>
+                <div className="divider_bottom"></div>
+                <div className="text_bottom">{props.tweets.length > 3 ? props.tweets[3].text : ''}</div>
+                <div className="Tweet_img_4"><img src={props.tweets.length > 3 ? props.tweets[3].user.profile_image_url_https : ''} alt="Profile Image"></img></div>
+            </div>
+
+            <div className="Tweet_5">
+                <div className="name_bottom">{props.tweets.length > 4 ? props.tweets[4].user.name : ''}</div>
+                <div className="screen_name_bottom">{props.tweets.length > 4 ? '@' + props.tweets[4].user.screen_name : ''}</div>
+                <div className="divider_bottom"></div>
+                <div className="text_bottom">{props.tweets.length > 4 ? props.tweets[4].text : ''}</div>
+                <div className="Tweet_img_5"><img src={props.tweets.length > 4 ? props.tweets[4].user.profile_image_url_https : ''} alt="Profile Image"></img></div>
+            </div>
+
+            <div className="Tweet_6">
+                <div className="name_bottom">{props.tweets.length > 5 ? props.tweets[5].user.name : ''}</div>
+                <div className="screen_name_bottom">{props.tweets.length > 5 ? '@' + props.tweets[5].user.screen_name : ''}</div>
+                <div className="divider_bottom"></div>
+                <div className="text_bottom">{props.tweets.length > 5 ? props.tweets[5].text : ''}</div>
+                <div className="Tweet_img_6"><img src={props.tweets.length > 5 ? props.tweets[5].user.profile_image_url_https : ''} alt="Profile Image"></img></div>
+            </div>
+
+        </div>
         <section className="modal_actions">
             {props.canClose && <button className="btn-modal" onClick={props.onClose}>Close</button>}
         </section>

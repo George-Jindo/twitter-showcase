3a9244a93d581668c5aa644cc7973e7154542ab0
George J <g.jindo@gmail.com>
2019-09-06 09:39:23 -0700

    added test button in RandomTweet page, connected to backend server

--- React/src/pages/RandomTweet.jsx
+++ React/src/pages/RandomTweet.jsx
@@ -1,12 +1,49 @@
-import React from "react";
+import React, { Component } from "react";
+import { Grid, Cell } from 'react-mdl';
+import axios from "axios";
+import favorites from "../favoriteUsers";
 
 
-const RandomTweet = () => {
-    return (
-        <div style={{marginTop: '75px', width: '100%'}}>
-            <h3>This is where the Random Tweets are!</h3>
-        </div>
-    );
-};
+class RandomTweet extends Component {
+    constructor() {
+        super();
+        this.state = {
+            users: favorites,
+            tweets: []
+        };
+    }
+
+    onClickHandler = () => {
+        axios.get('http://localhost:5000/api/random/')
+            .then(response => {
+                console.log('data: ', response.data.data);
+                //this.setState({ tweets: response.data });
+            })
+            .catch(error => {
+                console.log("Error Message: " + error);
+            });
+
+    };
+
+    render() {
+
+        return (
+            <div style={{ marginTop: '75px', width: '100%', height: '100%' }}>
+                <div className="favorite_users">
+
+                    <button
+                        className="individual_user"
+                        onClick={this.onClickHandler}
+                    >
+                        Test Button
+                    </button>
+
+                </div>
+
+            </div>
+
+        );
+    }
+}
 
 export default RandomTweet;

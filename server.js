504f324176084ef2488386dee20ed21e11ad60e9
George J <g.jindo@gmail.com>
2019-09-14 08:10:42 -0700

    Added random tweets being pulled from a count of 20 with count function on the BE

--- server.js
+++ server.js
@@ -1,57 +1,64 @@
-require("dotenv").config();
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const path = require('path');
 const axios = require('axios');
 const port = process.env.PORT || 5000;
 
-
 // Send a GET request to /api/tweets to VIEW a list of tweets
 app.get('/api/tweets/', (req, res) => {
     const url = `https://api.twitter.com/1.1/search/tweets.json?q=${req.query.q}&lang=en`;
 
-
     const config = {
-        headers: { 'Authorization': "Bearer " + process.env.REACT_APP_TWITTER_BEARER_TOKEN }
-    }
+        headers: {
+            Authorization:
+                'Bearer ' + process.env.REACT_APP_TWITTER_BEARER_TOKEN
+        }
+    };
 
-    axios.get(url, config)
-        .then((response) => {
+    axios
+        .get(url, config)
+        .then(response => {
             res.send(response.data.statuses);
             //console.log(response.data.statuses);
         })
-        .catch((error) => {
+        .catch(error => {
             res.sendStatus(500);
-        })
-
+        });
 });
-
 
 // Send a GET request to /api/tweets/tweet/random to VIEW a random tweet
 app.get('/api/random/', (req, res) => {
-    const url = `https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=${req.query.screen_name}&count=1`;
-
-
-    const config = {
-        headers: { 'Authorization': "Bearer " + process.env.REACT_APP_TWITTER_BEARER_TOKEN }
+    function count(max) {
+        return Math.floor(Math.random() * Math.floor(max));
     }
 
-    axios.get(url, config)
-        .then((response) => {
-            res.send(response.data[0]);
+    const url = `https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=${req.query.screen_name}&count=20`;
+
+    const config = {
+        headers: {
+            Authorization:
+                'Bearer ' + process.env.REACT_APP_TWITTER_BEARER_TOKEN
+        }
+    };
+
+    axios
+        .get(url, config, count)
+        .then(response => {
+            res.send(response.data[count(20)]);
             //console.log(response);
         })
-        .catch((error) => {
+        .catch(error => {
             res.sendStatus(500);
-        })
-
+        });
 });
-
-
 
 // serve static build files from React app
 app.use('/js', express.static(path.join(__dirname, 'react/build/js')));
-app.use('/manifest.json', express.static(path.join(__dirname, 'react/build/manifest.json')));
+app.use(
+    '/manifest.json',
+    express.static(path.join(__dirname, 'react/build/manifest.json'))
+);
 app.use('/media', express.static(path.join(__dirname, 'react/build/media')));
 app.use('/images', express.static(path.join(__dirname, 'react/build/images')));
 
@@ -59,7 +66,6 @@ app.use('/images', express.static(path.join(__dirname, 'react/build/images')));
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'React', 'build', 'index.html'));
 });
-
 
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));

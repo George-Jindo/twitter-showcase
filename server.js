a1e075aca0804dd7a0e68e6ce0e48a30129e5a5f
George J <g.jindo@gmail.com>
2019-09-15 20:45:35 -0700

    added cors library

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const app = express();
 const path = require('path');
 const axios = require('axios');
 const port = process.env.PORT || 5000;
+const cors = require('cors');
+
+app.use(cors());
 
 // Send a GET request to /api/tweets to VIEW a list of tweets
 app.get('/api/tweets/', (req, res) => {
@@ -84,3 +87,15 @@ if (process.env.NODE_ENV === 'production') {
 */
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
+
+// Allow credentials for CORS
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json'
+    );
+    next();
+});

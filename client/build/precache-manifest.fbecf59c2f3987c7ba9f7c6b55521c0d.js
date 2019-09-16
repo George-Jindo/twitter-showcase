c2e8102fcc990c7e99aaa4f32c84609c8461747a
George J <g.jindo@gmail.com>
2019-09-15 21:04:11 -0700

    ran build script

---
+++ client/build/precache-manifest.fbecf59c2f3987c7ba9f7c6b55521c0d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d42000cfccb5b5b7c5105543a18fdbc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0fa0190aea334f6ff39",
+    "url": "/static/css/main.ed41c6ac.chunk.css"
+  },
+  {
+    "revision": "8c104ca5ee19e424176a",
+    "url": "/static/js/2.43eca46b.chunk.js"
+  },
+  {
+    "revision": "e0fa0190aea334f6ff39",
+    "url": "/static/js/main.09f6dc57.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "623f6b1b848c9a04646261ae48ac1c01",
+    "url": "/static/media/birds.623f6b1b.jpg"
+  },
+  {
+    "revision": "879a806f6e29e0fd3cb0a325ea28c746",
+    "url": "/static/media/ts-png-8.879a806f.png"
+  }
+]);
